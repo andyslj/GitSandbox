575f40526b178dbe1b144f4c3830095015209218
Andy Shiue <anyd.shiue@animoto.com>
2012-06-05 22:31:32 -0400

    need to know the sand weight

--- sand.hpp
+++ sand.hpp
@@ -1,8 +1,9 @@
 struct sand
 {
-   sand(state s = DRY) : mState(s)
+   sand(state s = DRY, float w) : mState(s), mWeight(w)
    {
    }
    enum state { DRY, WET};
    state mState;
+   float mWeight;
 };
